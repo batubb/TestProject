05fe3227cbfd461843a925d32db1bf5135dd260b
batubb <batuhanberkbalci@gmail.com>
2016-09-14 11:48:37 +0300

    trying sth new

--- app/src/main/java/cs193a/stanford/edu/testproject/MainActivity.java
+++ app/src/main/java/cs193a/stanford/edu/testproject/MainActivity.java
@@ -15,38 +15,38 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void showPicture(View view) {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, 1);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK && null != data) {
-            Uri uri = data.getData();
-            String[] projection = {MediaStore.Images.Media.DATA};
-
-            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(projection[0]);
-            String filePath = cursor.getString(columnIndex);
-            cursor.close();
-            Bitmap image = BitmapFactory.decodeFile(filePath);
-
-            ImageView im = (ImageView)findViewById(R.id.image);
-            im.setImageBitmap(image);
-
-        }
-
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//    public void showPicture(View view) {
+//        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(intent, 1);
+//        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == 1 && resultCode == RESULT_OK && null != data) {
+//            Uri uri = data.getData();
+//            String[] projection = {MediaStore.Images.Media.DATA};
+//
+//            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+//            cursor.moveToFirst();
+//
+//            int columnIndex = cursor.getColumnIndex(projection[0]);
+//            String filePath = cursor.getString(columnIndex);
+//            cursor.close();
+//            Bitmap image = BitmapFactory.decodeFile(filePath);
+//
+//            ImageView im = (ImageView)findViewById(R.id.image);
+//            im.setImageBitmap(image);
+//
+//        }
+//
+//    }
 }
